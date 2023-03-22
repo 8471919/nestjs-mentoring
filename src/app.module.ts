3b7ad6c7ef21d94a8e0c3865fb5694d3625933cd
Michael <7471919@naver.com>
2023-03-22 18:03:13 +0900

    typeorm과 DB설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './res/user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        retryAttempts: 10,
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        database: 'study',
+        username: 'root',
+        password: '',
+        entities: [
+          path.join(__dirname, 'src/entities/**/*.entity.{js, ts}'),
+          path.join(__dirname, 'dist/entities/**/*.entity.{js, ts}'),
+        ],
+        synchronize: false,
+        logging: true,
+        timezone: 'local',
+      }),
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
