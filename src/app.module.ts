86dbcf0cf35433b805c76de20ec04aaaff2ab821
Michael <7471919@naver.com>
2023-04-02 14:35:13 +0900

    configModule과 dotenv설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,24 @@ import * as path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './res/user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
     TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        retryAttempts: 10,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        retryAttempts: configService.get('NODE_ENV') === 'prod' ? 10 : 1,
         type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        database: 'study',
-        username: 'root',
-        password: '',
+        host: configService.get('DB_HOST'),
+        port: Number(configService.get('DB_PORT')),
+        database: configService.get('DB_NAME'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
         entities: [
           path.join(__dirname, 'src/entities/**/*.entity.ts'),
           path.join(__dirname, 'dist/entities/**/*.entity.js'),
