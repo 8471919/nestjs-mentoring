5ebf556e53dbcd84332fd4ea26349e4d73de379e
Michael <7471919@naver.com>
2023-05-11 21:07:24 +0900

    Article CRUD 작성

---
+++ src/res/article/article.controller.ts
@@ -0,0 +1,73 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { ArticleService } from './article.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { User } from 'src/decorators/user.decorator';
+
+@Controller('article')
+export class ArticleController {
+  constructor(private readonly articleService: ArticleService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async createArticle(@Body() body, @User() user) {
+    const userId = user.id;
+
+    const title = body.title;
+    const content = body.content;
+
+    const article = await this.articleService.createArticle(
+      title,
+      content,
+      userId,
+    );
+
+    return article;
+  }
+
+  @Get('/:id')
+  async readArticle(@Param('id') id) {
+    const articleId = id;
+
+    const article = await this.articleService.getArticle(articleId);
+
+    return article;
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Put('/:id')
+  async updateArticle(@Param('id') id, @User() user, @Body() body) {
+    const userId = user.id;
+    const articleId = id;
+
+    const title = body.title;
+    const content = body.content;
+
+    const res = await this.articleService.modifyArticle(
+      userId,
+      articleId,
+      title,
+      content,
+    );
+
+    return res;
+  }
+
+  @Delete('/:id')
+  async deleteArticle(@Param('id') id, @User() user) {
+    const userId = user.id;
+    const articleId = id;
+
+    const res = await this.articleService.removeArticle(userId, articleId);
+
+    return res;
+  }
+}
